0f4809b27aaca71356f05faa5275e189b904153a
obaw <996067226@qq.com>
2017-05-02 20:52:02 +0800

    create project

---
+++ src/test/java/obaw/music/trumpet/TrumpetApplicationTests.java
@@ -0,0 +1,16 @@
+package obaw.music.trumpet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrumpetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
