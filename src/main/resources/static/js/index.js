e4e4d85a49fa5ba17567a714d36571a4eab9370d
obaw <996067226@qq.com>
2017-05-05 01:33:19 +0800

    update index.html

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,3 +1,26 @@
 /**
  * Created by xiaowei on 17-5-4.
  */
+$(function () {
+  /*菜单按钮*/
+  $(".menu-ico").click(function () {
+    //this
+    var menuico = $(this);
+    if (menuico.hasClass("active")) {
+      menuico.removeClass("active");
+    } else {
+      menuico.addClass("active");
+    }
+    //menu
+    var menu = $(".menu");
+    if (menu.hasClass("active")) {
+      menu.removeClass("active");
+    } else {
+      menu.addClass("active");
+    }
+  });
+  /*搜索*/
+  $(".search").click(function () {
+    $(".menu").addClass("active");
+  })
+});
